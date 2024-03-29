cf1e99abc1e685a011f2b9f4665907323e6dcc7f
BayramMusayev <bayrammusayev1992@gmail.com>
2024-03-29 17:18:19 +0400

    banner

--- pages/blog/Card.tsx
+++ pages/blog/Card.tsx
@@ -5,14 +5,14 @@ import user from '@/public/user.jpeg'
 
 const Card = () => {
     return (
-        <div className='w-[32%]'>
+        <div className='w-[90%] md:w-[45%] lg:w-[30%] ease-in'>
             <div className="w-full items-center shadow p-3 rounded-lg">
                 <div>
                     <Image className='w-full' src={img} height={240}
                         alt="Picture of the author" />
                 </div>
                 <div><a className='text-blue-300' href="#">Tecnoloji</a></div>
-                <p className='text-xl my-4'>The Impact of Technology on the Workplace: How Technology is Changing</p>
+                <p className='text-xl my-4 text-[#181A2A] font-semibold'>The Impact of Technology on the Workplace: How Technology is Changing</p>
                 <div className='flex justify-between'>
                     <Image className='rounded-full' src={user} alt="user" width={30} height={30} />
                     <p className='text-lg text-[#97989F]'>Tracey Wilson</p>
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -8,11 +8,18 @@ function index() {
     return (
         <div>
             <Banner />
-            <div className='w-[90%] m-auto gap-5 flex justify-between flex-wrap'>
+            <div className='w-[90%] m-auto gap-5 flex justify-center md:justify-between flex-wrap ease-in'>
                 {Array.from({ length: 9 }, (_, index) => (
                     <Card key={index} />
                 ))}
-
+            </div>
+            <button className="block hover:bg-blue-300 text-[#696A75] m-auto font-medium border my-10 text-base py-2 px-5 rounded hover:ease-in duration-300">
+                Load More
+            </button>
+            <div className="bg-[#E8E8EA] w-1/2 m-auto rounded-xl text-center p-3 mb-10 text-[#696A75]">
+                <p className='text-sm'>Advertisement</p>
+                <p className='text-xl font-semibold' >You can place ads</p>
+                <p className='text-lg'>750x100</p>
             </div>
         </div>
 
